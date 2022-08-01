d4edeaf356dbd6ea0e096d2168a3c0715ed08003
ricsanfre <ricsanfre@gmail.com>
2022-08-01 16:57:09 +0200

    Missing Autowired import

--- src/main/java/com/ricsanfre/picluster/fluent/multiline/ScheduleTask.java
+++ src/main/java/com/ricsanfre/picluster/fluent/multiline/ScheduleTask.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 
 @Component
